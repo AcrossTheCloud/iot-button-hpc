bcd97f6e11fd0a847656e8fd22bbb2c042d205cd
Matthew Berryman <matthew@acrossthecloud.net>
2017-06-21 14:29:54 +1000

    various serverless fixes

--- startHPC.js
+++ startHPC.js
@@ -48,16 +48,3 @@ exports.invoke = function(event, context, callback) {
 
    // Use callback() and return information to the caller.
 };
-
-exports.delete = function(event, context, callback) {
-  const cloudformation = new AWS.CloudFormation();
-  var params = {
-    StackName: 'MY_HPC',
-  ],
-  RoleARN: 'arn:aws:iam::'+process.env.AWS_ACCOUNT_ID+':role/startHPC'
-  };
-  cloudformation.deleteStack(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  });
-};
