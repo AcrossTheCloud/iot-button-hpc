90b8e8e985a404d78ca715e010d6513100d179d5
Matthew Berryman <matthew@acrossthecloud.net>
2017-06-21 13:19:19 +1000

    initial commit of code

---
+++ handlers.js
@@ -0,0 +1,63 @@
+const AWS = require('aws-sdk');
+
+exports.invoke = function(event, context, callback) {
+
+  const cloudformation = new AWS.CloudFormation();
+
+  var params = {
+    StackName: 'MY_HPC', /* required */
+    Capabilities: [
+      'CAPABILITY_IAM'
+    ],
+    DisableRollback: false,
+    NotificationARNs: [
+      process.env.NOTIFICATION_ARN
+    ],
+    OnFailure: 'DELETE',
+    Parameters: [
+      {
+        ParameterKey: 'AccessUsername',
+        ParameterValue: 'alces',
+        UsePreviousValue: false
+      },
+      {
+        ParameterKey: 'AccessKeyName',
+        ParameterValue: process.env.ACCESS_KEY_NAME,
+        UsePreviousValue: false
+      },
+      {
+        ParameterKey: ''
+      }
+    ],
+    RoleARN: 'arn:aws:iam::'+process.env.AWS_ACCOUNT_ID+':role/startHPC',
+    Tags: [
+      {
+        Key: 'client',
+        Value: 'testing'
+      }
+    ],
+    TemplateURL: process.env.TEMPLATE_URL,
+    TimeoutInMinutes: 0
+  };
+  cloudformation.createStack(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+
+  callback(null, "HPC started");
+
+   // Use callback() and return information to the caller.
+};
+
+exports.delete = function(event, context, callback) {
+  const cloudformation = new AWS.CloudFormation();
+  var params = {
+    StackName: 'MY_HPC',
+  ],
+  RoleARN: 'arn:aws:iam::'+process.env.AWS_ACCOUNT_ID+':role/startHPC'
+  };
+  cloudformation.deleteStack(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+};
